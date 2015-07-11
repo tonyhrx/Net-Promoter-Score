c884cc43352c822710b13d05ec0243519ecef5e0
Tony Horrocks <tony@fabriko.co.uk>
2015-07-11 13:14:41 +0100

    Initial commit

---
+++ CRM/Netpromoterscore/Form/Report/CRM/Report/Form/Member/Netpromoterscore.mgd.php
@@ -0,0 +1,21 @@
+<?php
+// This file declares a managed database record of type "ReportTemplate".
+// The record will be automatically inserted, updated, or deleted from the
+// database as appropriate. For more details, see "hook_civicrm_managed" at:
+// http://wiki.civicrm.org/confluence/display/CRMDOC42/Hook+Reference
+return array (
+  0 => 
+  array (
+    'name' => 'CRM_Netpromoterscore_Form_Report_CRM_Report_Form_Member_Netpromoterscore',
+    'entity' => 'ReportTemplate',
+    'params' => 
+    array (
+      'version' => 3,
+      'label' => 'CRM_Report_Form_Member_Netpromoterscore',
+      'description' => 'CRM_Report_Form_Member_Netpromoterscore (uk.co.fabriko.module.netpromoterscore)',
+      'class_name' => 'CRM_Netpromoterscore_Form_Report_CRM_Report_Form_Member_Netpromoterscore',
+      'report_url' => 'netpromoterscore',
+      'component' => 'CiviMember',
+    ),
+  ),
+);
